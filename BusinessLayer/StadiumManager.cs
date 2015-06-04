f5470400b6810d02e29130e1a94a87b4ddee7361
simatv15 <simon.atv.15@gmail.com>
2015-06-04 22:07:18 +0200

    initial commit

---
+++ BusinessLayer/StadiumManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class StadiumManager
+    {
+        public IList<string> GetStadiums()
+        {
+            IStadiumManager stadiumManager = new StadiumStubManager();
+            List<IStadium> stadiums = stadiumManager.GetStadiums();
+            List<string> stringStadiums = new List<string>();
+            foreach (IStadium stadium in stadiums)
+            {
+                stringStadiums.Add(stadium.ToString());
+            }
+            return stringStadiums;
+        }
+    }
+}
